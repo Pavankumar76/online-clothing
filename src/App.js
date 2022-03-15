0a335d31be3c2ce30094722c86bd3a17ed45c0e3
pavankumar76 <iampavankumar97@gmail.com>
2022-03-15 11:55:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './components/Home.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+    
+  );
+}
+
+export default App;
