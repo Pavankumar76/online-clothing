e78783eb15d4930d1021e34fc1e4e9c6e91e970d
PavanKumar76 <Iampavankumar97@gmail.com>
2022-04-11 11:40:28 +0530

    checkout items and cart total

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
@@ -8,12 +8,18 @@ import { CartContext } from '../../contexts/cart.context';
 
 const CartDropdown = () => {
     const {cartItems }=useContext(CartContext);
+
+    const navigate =useNavigate();
+
+    const goToCheckOutHandler =()=>{
+        navigate('/checkout');
+    }
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map((item) => (<CartItem key={item.id} cartItem={item}/>))}
             </div>
-            <Button>CheckOut</Button>
+            <Button onClick={goToCheckOutHandler}>CheckOut</Button>
         </div>
     );
 }
